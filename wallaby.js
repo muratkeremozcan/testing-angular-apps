9d6588d1665e8173f2fe90955f78699194fb023c
Ozcan <murat.ozcan@siemens.com>
2019-05-25 16:09:59 -0500

    ch3

--- wallaby.js
+++ wallaby.js
@@ -4,11 +4,16 @@ module.exports = function (w) {
     files: [
       './**/listing1.1.src.ts',
       './**/contact.ts',
+      './**/contacts.component.ts',
+      './**/**/contact-edit.component.ts'
+
     ],
 
     tests: [
       './**/listing1.3.spec.ts',
-      './**/listing2.3.spec.ts'
+      './**/listing2.3.spec.ts',
+      './**/contacts.component.spec.ts',
+      './**/**/contact-edit.component.spec.ts'
     ],
 
     env: {
